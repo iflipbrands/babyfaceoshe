c575361fd62a8446ed33be8443848a87a495dad9
Jasmine D Wilson <iflipbrands@gmail.com>
2026-08-29 11:48:23 -0700

    Add files via upload

---
+++ assets/js/content.js
@@ -0,0 +1,87 @@
+window.BABYFACE_CONTENT = {
+  "artist": {
+    "name": "BABYFACE OSHE"
+  },
+  "music": [
+    {
+      "title": "The Rough Draft SZN 1",
+      "type": "Latest Release",
+      "year": "2026",
+      "audio": "https://iflipbrands.github.io/babyfaceoshe/assets/audio/YOURE_WELCOME_KLICKAUD.mp3",
+      "cover": "https://github.com/iflipbrands/babyfaceoshe/blob/main/assets/images/artworks-ugh0vzEYqZcfFaTi-vCMYzQ-t500x500.jpg?raw=true",
+      "player": true
+    },
+    {
+      "title": "Waste My Time",
+      "type": "Single",
+      "year": "2026",
+      "audio": "https://iflipbrands.github.io/babyfaceoshe/assets/audio/Waste_My_Time_BabyFaceOshe.mp3",
+      "cover": "https://github.com/iflipbrands/babyfaceoshe/blob/main/assets/images/artworks-XPl3WS1QxSz2IGo3-Qdc77A-t500x500.jpg",
+      "player": true
+    },
+    {
+      "title": "Heartless",
+      "type": "Single",
+      "year": "2026",
+      "audio": "https://github.com/iflipbrands/babyfaceoshe/blob/main/assets/audio/Heartless_KLICKAUD.mp3",
+      "cover": "https://github.com/iflipbrands/babyfaceoshe/blob/main/assets/images/artworks-DkCqweJ8RZOUY2Il-DUCqOA-t1080x1080.jpg",
+      "player": true
+    },
+    {
+      "title": "F.W.A.N.  ft ManMan",
+      "type": "Remix",
+      "year": "2026",
+      "audio": "https://github.com/iflipbrands/babyfaceoshe/blob/main/assets/audio/FWAN_KLICKAUD.mp3",
+      "cover": "https://github.com/iflipbrands/babyfaceoshe/blob/main/assets/images/artworks-z4mSn4yqAXmR52C6-Cqm8JA-t500x500.jpg",
+      "player": true
+    }
+  ],
+  "videos": [
+    {
+      "title": "Steppa Muzik",
+      "embed": "https://www.youtube.com/watch?v=6B2xU8KFnuA"
+    },
+    {
+      "title": "Demon Flow",
+      "embed": "https://www.youtube.com/watch?v=wgEJBhA3hPI"
+    }
+  ],
+  "tour": [
+    {
+      "date": "SEP 18",
+      "city": "LAS VEGAS, NV",
+      "venue": "The Backroom",
+      "price": "$15",
+      "buttonLabel": "Buy Tickets",
+      "link": "#"
+    },
+    {
+      "date": "OCT 03",
+      "city": "LOS ANGELES, CA",
+      "venue": "Warehouse 17",
+      "price": "Free",
+      "buttonLabel": "More Details",
+      "link": "#"
+    },
+    {
+      "date": "OCT 24",
+      "city": "PHOENIX, AZ",
+      "venue": "The Yard",
+      "price": "$5",
+      "buttonLabel": "Buy Tickets",
+      "link": "#"
+    }
+  ],
+  "about": {
+    "heading": "BabyFace Oshe",
+    "body": "BabyFace Oshe blends sharp perspective, confident delivery and a distinct street-rooted sound into records built to connect in real time. From the studio to the stage, the focus stays on the music, the movement and the audience.",
+    "quote": "Redlands, CA"
+  },
+  "socials": {
+    "facebook": "#",
+    "instagram": "#",
+    "tiktok": "#",
+    "soundcloud": "#",
+    "youtube": "#"
+  }
+};
